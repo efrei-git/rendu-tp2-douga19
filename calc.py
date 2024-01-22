ca150485460208b1b7ea396aa7369d854c653604
Rado Rakotonarivo <rado.raktn@gmail.com>
2024-01-22 13:41:07 +0100

    Added divide function

--- calc.py
+++ calc.py
@@ -17,5 +17,7 @@ from operation import *
                 print(num1, "-", num2, "=", subtract(num1, num2))
             elif operation == '*':
                 print(num1, "*", num2, "=", multiply(num1, num2))
+            elif operation == '/':
+                print(num1, "/", num2, "=", divide(num1, num2))
             else:
                 print("Invalid operation")
