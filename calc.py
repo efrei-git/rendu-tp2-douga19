9ed1cffafc73b61b28ffbcef811e6744f539259b
Rado Rakotonarivo <rado.raktn@gmail.com>
2024-01-22 14:42:19 +0100

    Fixed divide function with git checkout

--- calc.py
+++ calc.py
@@ -1,23 +1,25 @@
 from operation import *
 
-    if __name__ == "__main__":
-        while True:
-            input_str = input("Enter the calculation (in the format 'num1 operation num2', or 'exit' to exit): ")
+if __name__ == "__main__":
+    while True:
+        input_str = input("Enter the calculation (in the format 'num1 operation num2', or 'exit' to exit): ")
 
-            if input_str == "exit":
-                break
+        if input_str == "exit":
+            break
 
-            num1, operation, num2 = input_str.split()
-            num1 = int(num1)
-            num2 = int(num2)
+        num1, operation, num2 = input_str.split()
+        num1 = int(num1)
+        num2 = int(num2)
 
-            if operation == '+':
-                print(num1, "+", num2, "=", add(num1, num2))
-            elif operation == '-':
-                print(num1, "-", num2, "=", subtract(num1, num2))
-            elif operation == '*':
-                print(num1, "*", num2, "=", multiply(num1, num2))
-            elif operation == '/':
-                print(num1, "/", num2, "=", divide(num1, num2))
-            else:
-                print("Invalid operation")
+        if operation == '+':
+            print(num1, "+", num2, "=", add(num1, num2))
+        elif operation == '-':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+        elif operation == '*':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+        elif operation == '/':
+            result = divide(num1, num2)
+            if result is not None:
+                print(num1, "/", num2, "=", result)
+        else:
+            print("Invalid operation")
--- operation.py
+++ operation.py
@@ -8,4 +8,8 @@ def multiply(a, b):
         return a * b
 
 def divide(a, b):
-        return a / b
+    try:
+        ret = a / b
+    except ZeroDivisionError:
+        print("Division by zero is not allowed")
+        return None
